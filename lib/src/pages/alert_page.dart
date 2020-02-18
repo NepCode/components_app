1d64bd754d2cb767aa1aee1f86cca1609558180b
Antonio Aguila <desarrollo5@motidigital.com>
2020-02-17 20:19:39 -0600

    alert page

--- lib/src/pages/alert_page.dart
+++ lib/src/pages/alert_page.dart
@@ -9,6 +9,15 @@ class AlertPage extends StatelessWidget {
       appBar: AppBar(
         title: Text('Alert Page'),
       ),
+      body: Center(
+        child: RaisedButton(
+          onPressed: () => _showAlert(context),
+          child: Text('Show alert'),
+          color: Colors.blue,
+          textColor: Colors.white,
+          shape: StadiumBorder(),
+        ),
+      ),
       floatingActionButton: FloatingActionButton(
         child: Icon(Icons.add_location),
         onPressed: () {
@@ -17,4 +26,33 @@ class AlertPage extends StatelessWidget {
       ),
     );
   }
+
+
+  void _showAlert(BuildContext context) {
+
+    showDialog(
+      context: context,
+      barrierDismissible: true,
+      builder: (contex) {
+        return AlertDialog(
+          shape: RoundedRectangleBorder( borderRadius: BorderRadius.circular(20.0)),
+          title: Text('Title'),
+          content: Column(
+            mainAxisSize: MainAxisSize.min,
+            children: <Widget>[
+              Text('Alert Content'),
+              FlutterLogo( size: 100.0,)
+            ],
+          ),
+          actions: <Widget>[
+            FlatButton(onPressed: () => Navigator.of(context).pop(), child: Text('Cancel')),
+            FlatButton(onPressed: () {Navigator.of(context).pop(); }, child: Text('OK'))
+          ],
+        );
+      }
+    );
+
+  }
+
+
 }
